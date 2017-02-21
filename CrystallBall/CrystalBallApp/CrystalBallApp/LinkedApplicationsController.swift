83b6ab6debce758ce10e96f698745650675b742f
Kalyani  Mashiwal <kalyani.masiwal@gmail.com>
2017-02-21 14:36:28 +0530

    Add files via upload

--- CrystallBall/CrystalBallApp/CrystalBallApp/LinkedApplicationsController.swift
+++ CrystallBall/CrystalBallApp/CrystalBallApp/LinkedApplicationsController.swift
@@ -9,35 +9,39 @@
 import UIKit
 
 
-class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate{
     
     
     @IBOutlet weak var tableView: UITableView!
     
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    
+    var searchActive : Bool = false
+    var filtered:[String] = []
+    
     let linkedApplicationsURL = "http://10.0.1.210:8080/MaasOauthServer/rest/oauth/getAllApplications"
+    let linkedApplicationsCellIdentifier = "linkedApplicationCell"
     var firstApplication: String?
     var secondApplication: String?
     var thirdApplication: String?
     var fourthApplication: String?
-    
     var allAplications:[String] = []
-    
-    var items: [String] = []
-    
-       var linkedApps: [LinkedAllApplicationsVO] = []
-    var linkedApplication = LinkedAllApplicationsVO()
-       var tableName = [String]()
-      override func viewDidLoad() {
+    var tableName = [String]()
+    override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        //searchBar.delegate = self
         getAllCrystalBallLinkedApplications()
-               tableView.register(UITableViewCell.self,
-                           forCellReuseIdentifier: "linkedApplicationCell")
-    
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: linkedApplicationsCellIdentifier)
+        
     }
-
-    //
+    
+    // Web Services to get all applications
     func getAllCrystalBallLinkedApplications(){
         var request = URLRequest(url: URL(string: linkedApplicationsURL)!)
         request.httpMethod = "POST"
@@ -55,61 +59,93 @@ class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITab
                 if let firstApplicationNumbers = jsonDict["1"] as? NSDictionary {
                     if let firstApp = firstApplicationNumbers["_id"] as? String {
                         self.firstApplication = firstApp
-                        print("first app------\(self.firstApplication)")
                         self.allAplications.append(firstApp)
                     }
                 }
                 if let secondApplicationNumbers = jsonDict["2"] as? NSDictionary {
                     if let secondApp = secondApplicationNumbers["_id"] as? String {
                         self.secondApplication = secondApp
-                        print("second app------\(self.secondApplication)")
                         self.allAplications.append(secondApp)
                     }
                 }
                 if let thirdApplicationNumbers = jsonDict["3"] as? NSDictionary {
                     if let thirdApp = thirdApplicationNumbers["_id"] as? String {
                         self.thirdApplication = thirdApp
-                        print("third app------\(self.thirdApplication)")
                         self.allAplications.append(thirdApp)
                     }
                 }
                 if let fourthApplicationNumbers = jsonDict["4"] as? NSDictionary {
                     if let fourthApp = fourthApplicationNumbers["_id"] as? String {
                         self.fourthApplication = fourthApp
-                        print("fourth app------\(self.fourthApplication)")
                         self.allAplications.append(fourthApp)
                         
                     }
                 }
-                
-                print(self.allAplications)
-                 DispatchQueue.main.async {
-               
-                   self.tableName = self.allAplications
-                  
-                DispatchQueue.main.async{
-                    self.tableView.reloadData()
+                DispatchQueue.main.async {
+                    self.tableName = self.allAplications
+                    
+                    DispatchQueue.main.async{
+                        self.tableView.reloadData()
                     }
                 }
-        }
+            }
         }
         task.resume()
     }
     
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchActive = true;
+    }
     
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filtered = tableName.filter({ (text) -> Bool in
+            let tmp: NSString = text as NSString
+            let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
+            return range.location != NSNotFound
+        })
+        if(filtered.count == 0){
+            searchActive = false;
+        } else {
+            searchActive = true;
+        }
+        self.tableView.reloadData()
+    }
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return tableName.count
+        return tableName.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "linkedApplicationCell", for: indexPath as IndexPath)
-        cell.textLabel?.text = tableName[indexPath.item]
+        let cell = tableView.dequeueReusableCell(withIdentifier: linkedApplicationsCellIdentifier, for: indexPath as IndexPath)
+        cell.textLabel?.textAlignment = .center
+        //cell.backgroundColor = UIColor.blue
+         cell.textLabel?.text = tableName[indexPath.item]
+//        if(searchActive)
+//        {
+//           // cell.textLabel?.text = filtered[indexPath.row]
+//        } else {
+//            cell.textLabel?.text = tableName[indexPath.row];
+//        }
+
         return cell
     }
-
-    }
+    
+}
 
 
 
