043e669d517e39deecded4997073bc21789964bf
Kalyani  Mashiwal <kalyani.masiwal@gmail.com>
2017-03-02 10:22:38 +0530

    Add files via upload

--- LinkedApplicationsController.swift
+++ LinkedApplicationsController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+import CoreData
 
 class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate{
     
@@ -21,10 +22,10 @@ class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITab
     
     @IBOutlet var menuBarView: UIView!
     
-   
+    
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
     
-  
+    
     var menuShowing = false
     
     @IBOutlet weak var menuBarLoginUserNameLabel: UILabel!
@@ -38,10 +39,13 @@ class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITab
     var fourthApplication: String?
     var allAplications:[String] = []
     var tableName = [String]()
-    
     var searchActive : Bool = false
     var filtered:[String] = []
-   var delegate: UIViewController?
+    var delegate: UIViewController?
+    var applicationName = [String]()
+    // Entity , which is added into database
+    
+    var cbUser: [CBUser] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         //set login user name in a label
@@ -54,7 +58,7 @@ class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITab
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.tap(_:)))
         menuBarView.addGestureRecognizer(tap)
         tap.cancelsTouchesInView = false
-        getAllCrystalBallLinkedApplications()
+       // getAllCrystalBallLinkedApplications()
         tableView.register(UITableViewCell.self,
                            forCellReuseIdentifier: LINKED_APPLICATIONS_CELL_IDENTIFIER)
         for subView in self.searchBar.subviews
@@ -68,7 +72,14 @@ class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITab
                     textField.textColor = UIColor.white                }
             }
         }
-      }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        // get the data from core data
+        getApplicationSelectedByUser()
+        // reload the table view
+        tableView.reloadData()
+    }
+    
     //Search Bar functionality
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         searchActive = true;
@@ -87,8 +98,9 @@ class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITab
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        
-        filtered = tableName.filter({ (text) -> Bool in
+       applicationName = [getApplicationNameFromCoreData(loginUser: loginUserName)]
+      
+        filtered = applicationName.filter({ (text) -> Bool in
             let tmp: NSString = text as NSString
             let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
             return range.location != NSNotFound
@@ -152,13 +164,65 @@ class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITab
         }
         task.resume()
     }
-    
+    // Receive data from database
+    func getApplicationSelectedByUser()
+    {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do
+        {
+        let request : NSFetchRequest<CBUser> = CBUser.fetchRequest()
+           
+        
+            let dataPredicate = NSPredicate(format: "cbUsername == %@", loginUserName)
+      
+            request.predicate = dataPredicate
+         
+            cbUser  = try context.fetch(request)
+            
+            
+            
+        }
+        catch
+        {
+            print("Fetching Failed")
+        }
+        
+    }
+    // delete or edit data
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        if editingStyle == .delete
+        {
+            let finalTask = cbUser[indexPath.row]
+            context.delete(finalTask)
+            
+            // now save the context
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            // reload the data
+            
+            do
+            {
+                cbUser = try context.fetch(CBUser.fetchRequest())
+                
+            }
+            catch
+            {
+                print("Error in deletion of data")
+            }
+            
+        }
+        // reload the data after deleting
+        tableView.reloadData()
+        
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if(searchActive) {
             return filtered.count
         }
-        return tableName.count
+        return cbUser.count
     }
     // open an alertView while clicking on cell
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -171,11 +235,13 @@ class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITab
         
         let cell = tableView.dequeueReusableCell(withIdentifier: LINKED_APPLICATIONS_CELL_IDENTIFIER, for: indexPath as IndexPath)
         self.tableView.separatorColor = UIColor.white
-        
+        let crystalBallUsers = cbUser[indexPath.row]
+
         if(searchActive){
             cell.textLabel?.text = filtered[indexPath.row]
         } else {
-            cell.textLabel?.text = tableName[indexPath.row];
+            cell.textLabel?.text = crystalBallUsers.value(forKeyPath: "applicationName") as? String
+             //self.tableName = crystalBallUsers.value(forKeyPath: "applicationName") as! [String]
         }
         // NOTIFICATION ICON PART 1 BELL
         
@@ -198,9 +264,9 @@ class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITab
             cell.imageView?.image = UIImage(named: "LOGO 4.png")
         }
         
-         return cell
+        return cell
     }
-  
+    
     //Menu Bar functionality
     
     @IBAction func menuBtnPressed(_ sender: Any) {
@@ -218,6 +284,47 @@ class LinkedApplicationsController: UIViewController, UITableViewDelegate, UITab
         leadingConstraint.constant = -140
         
     }
+    
+    // get application name
+    
+    func getApplicationNameFromCoreData(loginUser : String) -> String
+    {
+        var applicationName : String = ""
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do
+        {
+            let request : NSFetchRequest<CBUser> = CBUser.fetchRequest()
+         
+            let dataPredicate = NSPredicate(format: "cbUsername == %@", loginUser)
+             request.predicate = dataPredicate
+            
+            cbUser  = try context.fetch(request)
+            
+            for userDetails in cbUser as [NSManagedObject] {
+                //get the Key Value pairs (although there may be a better way to do that...
+                applicationName = userDetails.value(forKey: "applicationName") as! String
+               
+            }
+       
+        }
+        catch
+        {
+            print("Fetching application name failed")
+        }
+       
+        return applicationName
+    }
+    
+    // Link new application button clicked
+    
+    
+    @IBAction func linkNewApplicationBtnPressed(_ sender: Any) {
+        
+        
+        
+        
+    }
+    
 }
 
 
